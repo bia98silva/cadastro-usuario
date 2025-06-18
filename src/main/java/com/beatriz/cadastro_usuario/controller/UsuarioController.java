29cf5fc0e2bdeaa40ec0860f8d97eff1a9d474db
Beatriz Silva <beatrizsierra98@hotmail.com>
2025-06-17 22:22:16 -0300

    Initial commit

---
+++ src/main/java/com/beatriz/cadastro_usuario/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.beatriz.cadastro_usuario.controller;
+
+
+import com.beatriz.cadastro_usuario.business.UsuarioService;
+import com.beatriz.cadastro_usuario.infraestructure.entitys.Usuario;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
+        usuarioService.SalvarUsuario(usuario);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam String email) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarUsuarioPorEmail(@RequestParam String email){
+        usuarioService.deletarUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+    @PutMapping
+    public ResponseEntity<Void> atualizarUsuarioPorId(@RequestParam Integer id,
+                                                      @RequestBody Usuario usuario){
+        usuarioService.atualizarUsuarioPorId(id, usuario);
+        return ResponseEntity.ok().build();
+    }
+}
